3b8fe0f914086b5eb11a74fa19197ce642e09e10
Shiranaiwayo <susanne.j.r@me.com>
2026-09-15 16:59:00 +0200

    Rewritten to make sure every input is valid

--- Program.cs
+++ Program.cs
@@ -4,53 +4,50 @@
     {
         static void Main(string[] args)
         {
-            int randomNum = new Random().Next(1, 50);
+            int randomNum = new Random().Next(1, 51);
 
             Console.Write("Hi! Let's play a game! Guess a number between 1 and 50: ");
-            string input = Console.ReadLine();
+            int guess = GetValidGuess();
 
-            int guess;
+            int totalTries = 10;
 
-            if (CheckIfInt(ref input)) ;
+
+            for (int guesses = 1; guesses < totalTries && guess != randomNum; guesses++)
             {
-                guess = (Convert.ToInt32(input));
-            }
-
-
-            if (guess < 1 || guess > 50 || !int.TryParse(input, out _))
-            {
-                Console.Write("Please enter a number greater than 0 and less than 51: ");
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
-
-            for (int guesses = 1; guesses < 10 && guess != randomNum; guesses++)
-            {
-                Console.Write("Wrong guess! " + (10 - guesses) + " tries left. Try again: ");
-                guess = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Wrong guess! {totalTries - guesses} tries left. Try again: ");
+                guess = GetValidGuess();
             }
 
             if (guess == randomNum)
             {
-                Console.WriteLine("Congratulations! You guessed the number: " + randomNum);
+                Console.WriteLine($"Congratulations! You guessed the number: {randomNum}");
             }
             else
             {
-                Console.WriteLine("Sorry! You ran out of tries! The number was: " + randomNum);
+                Console.WriteLine($"Sorry! You ran out of tries! The number was: {randomNum}");
             }
         }
-        private static bool CheckIfInt(ref string input)
+        //Helper method to get a valid guess from the user
+        private static int GetValidGuess()
         {
-            if (int.TryParse(input, out int _))
+            while (true)
             {
-                return true;
+                string input = Console.ReadLine();
+                //Check if it's a valid number
+                if (!int.TryParse(input, out int parsedGuess))
+                {
+                    Console.Write("Please enter a valid number: ");
+                    continue; //restarts the loop to ask again
+                }
+                //Check if it's within the right range
+                if (parsedGuess < 1 || parsedGuess > 50)
+                {
+                    Console.Write("Please enter a number between 1 and 50: ");
+                    continue; //restarts the loop to ask again
+                }
+                //If it passes both checks, return the valid guess
+                return parsedGuess;
             }
-            else
-            {
-                Console.Write("Please enter a valid number: ");
-                input = (Console.ReadLine());
-            }
-
-            return true;
         }
     }
 }
