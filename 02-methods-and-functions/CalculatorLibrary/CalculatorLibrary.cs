40da5f33a0ee510faea13526b40a51da788dc89b
Shiranaiwayo <susanne.j.r@me.com>
2026-09-18 16:11:58 +0200

    Fix: Remove embedded git repos and add projects correctly

---
+++ 02-methods-and-functions/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Starting Calculator Log");
+            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+        }
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; //Default value is "not-a-number", if an operation, such as division, could result in an error.
+            //Do the math with a switch statement
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "d":
+                    //ask the user to enter a non-zero divisor
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    }
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid operation.");
+            }
+            return result;
+        }
+    }
+
+}
